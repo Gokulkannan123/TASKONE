804ceb69df90e325f6245f468d2b35172357c5d4
minion12309 <gokulkannan.s@finstein.ai>
2022-09-22 14:38:11 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,20 +6,34 @@ import { Component2Component } from './component2/component2.component';
 import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [{
-  path:'aboutus',
-  component:AboutusComponent
+  path: 'aboutus',
+  component: AboutusComponent,
+  children: [{
+    path: 'component1',
+    component: Component1Component,
+    children: [{
+      path: 'component2',
+      component: Component2Component,
+      children:[{
+        path:'contactus',
+        component:ContactusComponent
+       }]
+    }],
+    
+  }
+  ]
 },
 {
-  path:'contactus',
-  component:ContactusComponent
+  path: 'contactus',
+  component: ContactusComponent
 },
 {
-  path:'component1',
-  component:Component1Component
+  path: 'component1',
+  component: Component1Component
 },
 {
-  path:'component',
-  component:Component2Component
+  path: 'component2',
+  component: Component2Component
 }
 ];
 @NgModule({
